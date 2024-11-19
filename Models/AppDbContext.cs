9d8032f6206c3c10d73dae0d0af11c2d0fee5e94
mykola.grygorenko <grygorennik@gmail.com>
2024-11-19 13:10:28 +0100

    lab7

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Laboratorium1.Models;
 
-public class AppDbContext: DbContext
+public class AppDbContext: IdentityDbContext<IdentityUser>
 {
     public DbSet<ContactEntity> Contacts {
         get;
@@ -25,6 +27,68 @@ public class AppDbContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        
+        string  ADMIN_ID = Guid.NewGuid().ToString();
+        string USER_ID = Guid.NewGuid().ToString();
+
+        modelBuilder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole()
+                {
+                    Id = ADMIN_ID,
+                    Name = "admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = ADMIN_ID
+                },
+                new IdentityRole()
+                {
+                    Id = USER_ID,
+                    Name = "user",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = USER_ID
+                }
+            );
+        var admin = new IdentityUser()
+        {
+            Id = ADMIN_ID,
+            UserName = "Adam",
+            NormalizedUserName = "Adam",
+            Email = "adam@wsei.edu.pl",
+            NormalizedEmail = "ADAM@WSEI.EDU.PL",
+            EmailConfirmed = true
+        };
+        var user = new IdentityUser()
+        {
+            Id = USER_ID,
+            UserName = "John",
+            NormalizedUserName = "JOHN",
+            Email = "john@wsei.edu.pl",
+            NormalizedEmail = "JOHN@WSEI.EDU.PL",
+            EmailConfirmed = true
+        };
+        
+        PasswordHasher<IdentityUser> hasher = new PasswordHasher<IdentityUser>();
+        admin.PasswordHash = hasher.HashPassword(admin, "1234");
+        user.PasswordHash = hasher.HashPassword(user, "abcd");
+
+        modelBuilder.Entity<IdentityUser>()
+            .HasData(admin, user);
+
+        modelBuilder.Entity<IdentityUserRole<string>>()
+            .HasData(
+                new IdentityUserRole<string>()
+                {
+                    RoleId = ADMIN_ID,
+                    UserId = ADMIN_ID
+                },
+                new IdentityUserRole<string>()
+                {
+                    RoleId = USER_ID,
+                    UserId = USER_ID
+                }
+            );
+           
         modelBuilder.Entity<OrganizationEntity>()
             .OwnsOne(o => o.Address)
             .HasData(
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Laboratorium1.Models;
 using Laboratorium1.Models.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Laboratorium1;
 
@@ -8,11 +10,24 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
         // Add services to the container.
         builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
         builder.Services.AddDbContext<AppDbContext>();
+        builder.Services.AddDefaultIdentity<IdentityUser>(o =>
+            {
+                o.SignIn.RequireConfirmedAccount = true;
+                o.Password.RequiredLength = 5;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequiredUniqueChars = 0;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>();
+       
         builder.Services.AddTransient<IContactService, EFContactService>();
+        builder.Services.AddMemoryCache();
+        builder.Services.AddSession();
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -28,8 +43,10 @@ public class Program
 
         app.UseRouting();
 
+        app.UseAuthentication();
+        app.UseSession();
         app.UseAuthorization();
-
+        app.MapRazorPages();
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
